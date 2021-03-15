211ddf29c608ae7ff1344dd16d8d001b57e02e70
Paulo Daniel Gonzalez <pdgonzalez872@gmail.com>
2021-03-15 14:41:24 -0500

    Add tmux script

---
+++ tmux_create_windows.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+#
+# Use this when you want to create a bunch of tmux windows, like I like to.
+#
+
+tmux start
+tmux new-session -d -t monitor
+
+tmux new-window -t monitor
+tmux new-window -t monitor
+tmux new-window -t monitor
+tmux new-window -t monitor
+tmux new-window -t monitor
+tmux new-window -t monitor
+tmux new-window -t monitor
+tmux new-window -t monitor
+tmux new-window -t monitor
+
+tmux rename-window -t monitor:0 'main'
+tmux rename-window -t monitor:1 'vim'
+tmux rename-window -t monitor:2 'console'
+tmux rename-window -t monitor:3 'tests'
+tmux rename-window -t monitor:4 'server'
+tmux rename-window -t monitor:5 'docker'
+tmux rename-window -t monitor:6 'bash'
+tmux rename-window -t monitor:7 'logs'
+tmux rename-window -t monitor:8 'bash'
+tmux rename-window -t monitor:9 'psql'
