8610b32c57edf8f48d2cb1eeb450d94b3abcd138
Paulo Daniel Gonzalez <pdgonzalez872@gmail.com>
2021-03-15 18:17:32 -0500

    the lns

---
+++ ln_for_app_user.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+
+sudo ln -s /usr/bin/whoami /home/app/bin/
+sudo ln -s /usr/bin/readlink /home/app/bin
+sudo ln -s /usr/bin/dirname /home/app/bin
+sudo ln -s /usr/bin/cut /home/app/bin
+sudo ln -s /usr/bin/sed /home/app/bin
+sudo ln -s /usr/bin/cat /home/app/bin
+sudo ln -s /usr/bin/grep /home/app/bin
+sudo ln -s /usr/bin/basename /home/app/bin
